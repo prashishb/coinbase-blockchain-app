b7181aae4e298ed0298ebe2864e649434b33898b
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2022-01-22 19:06:16 -0500

    built coin portfolio

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../components/Header';
+import Main from '../components/Main';
 
 const Dashboard = ({ address }) => {
   return (
     <Wrapper>
       <MainContainer>
         <Header />
+        <Main />
       </MainContainer>
     </Wrapper>
   );
